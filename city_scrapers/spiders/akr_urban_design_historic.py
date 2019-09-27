dcb3fe4b021bffcb71cd6bedaf23ae00b718c4d9
pjsier <pjsier@gmail.com>
2019-09-27 15:52:29 -0500

    Add akr_urban_design_historic

---
+++ city_scrapers/spiders/akr_urban_design_historic.py
@@ -0,0 +1,65 @@
+import re
+from datetime import datetime
+from io import BytesIO
+
+from city_scrapers_core.constants import COMMISSION
+from city_scrapers_core.items import Meeting
+from city_scrapers_core.spiders import CityScrapersSpider
+from PyPDF2 import PdfFileReader
+
+
+class AkrUrbanDesignHistoricSpider(CityScrapersSpider):
+    name = "akr_urban_design_historic"
+    agency = "Akron Urban Design and Historic Preservation"
+    timezone = "America/Detroit"
+    allowed_domains = ["www.akronohio.gov"]
+    start_urls = ["https://www.akronohio.gov/cms/site/4820d164c8ec21ed/index.html"]
+    location = {
+        "name": "Akron City Hall",
+        "address": "166 S High St, Akron, OH 44308",
+    }
+
+    def parse(self, response):
+        """
+        `parse` should always `yield` Meeting items.
+
+        Change the `_parse_title`, `_parse_start`, etc methods to fit your scraping
+        needs.
+        """
+        for link in response.css("#mainColumn a"):
+            if "Calendar" in " ".join(link.css("*::text").extract()):
+                yield response.follow(
+                    link.attrib["href"], callback=self._parse_calendar, dont_filter=True
+                )
+
+    def _parse_calendar(self, response):
+        """Parse dates and details from schedule PDF"""
+        pdf_obj = PdfFileReader(BytesIO(response.body))
+        pdf_text = re.sub(r"\s+", " ", pdf_obj.getPage(0).extractText()).replace(" ,", ",")
+
+        for idx, date_str in enumerate(re.findall(r"[a-zA-Z]{3,10} \d{1,2}, \d{4}", pdf_text)):
+            # Ignore every other item
+            if idx % 2 == 1:
+                continue
+            meeting = Meeting(
+                title="Urban Design and Historic Preservation Commission",
+                description="",
+                classification=COMMISSION,
+                start=self._parse_start(date_str),
+                end=None,
+                all_day=False,
+                time_notes="Confirm details with agency",
+                location=self.location,
+                links=[],
+                source=self.start_urls[0],
+            )
+
+            meeting["status"] = self._get_status(meeting)
+            meeting["id"] = self._get_id(meeting)
+
+            yield meeting
+
+    def _parse_start(self, date_str):
+        """Parse start datetime as a naive datetime object."""
+        # TODO: Find start time
+        return datetime.strptime(date_str.title(), "%B %d, %Y")
---
+++ tests/test_akr_urban_design_historic.py
@@ -0,0 +1,77 @@
+from datetime import datetime
+from os.path import dirname, join
+
+import pytest  # noqa
+from city_scrapers_core.constants import COMMISSION, PASSED
+from city_scrapers_core.utils import file_response
+from freezegun import freeze_time
+
+from city_scrapers.spiders.akr_urban_design_historic import AkrUrbanDesignHistoricSpider
+
+test_response = file_response(
+    join(dirname(__file__), "files", "akr_urban_design_historic.pdf"),
+    url="https://www.akronohio.gov/cms/UDHP2019/2019__udhpc.pdf",
+    mode="rb",
+)
+spider = AkrUrbanDesignHistoricSpider()
+
+freezer = freeze_time("2019-09-27")
+freezer.start()
+
+parsed_items = [item for item in spider._parse_calendar(test_response)]
+
+freezer.stop()
+
+
+def test_count():
+    assert len(parsed_items) == 12
+
+
+def test_title():
+    assert parsed_items[0]["title"] == "Urban Design and Historic Preservation Commission"
+
+
+def test_description():
+    assert parsed_items[0]["description"] == ""
+
+
+def test_start():
+    assert parsed_items[0]["start"] == datetime(2019, 1, 8, 0, 0)
+
+
+def test_end():
+    assert parsed_items[0]["end"] is None
+
+
+def test_time_notes():
+    assert parsed_items[0]["time_notes"] == "Confirm details with agency"
+
+
+def test_id():
+    assert parsed_items[0][
+        "id"
+    ] == "akr_urban_design_historic/201901080000/x/urban_design_and_historic_preservation_commission"  # noqa
+
+
+def test_status():
+    assert parsed_items[0]["status"] == PASSED
+
+
+def test_location():
+    assert parsed_items[0]["location"] == spider.location
+
+
+def test_source():
+    assert parsed_items[0]["source"] == spider.start_urls[0]
+
+
+def test_links():
+    assert parsed_items[0]["links"] == []
+
+
+def test_classification():
+    assert parsed_items[0]["classification"] == COMMISSION
+
+
+def test_all_day():
+    assert parsed_items[0]["all_day"] is False
