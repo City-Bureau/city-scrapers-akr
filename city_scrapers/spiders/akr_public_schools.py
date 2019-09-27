640928cce3908974615b1c5719f2cfa633e3383c
pjsier <pjsier@gmail.com>
2019-09-27 09:41:27 -0500

    Add akr_public_schools

---
+++ city_scrapers/spiders/akr_public_schools.py
@@ -0,0 +1,138 @@
+import re
+from datetime import datetime
+
+from city_scrapers_core.constants import BOARD, COMMITTEE
+from city_scrapers_core.items import Meeting
+from city_scrapers_core.spiders import CityScrapersSpider
+from scrapy import Selector
+
+
+class AkrPublicSchoolsSpider(CityScrapersSpider):
+    name = "akr_public_schools"
+    agency = "Akron Public Schools"
+    timezone = "America/Detroit"
+    allowed_domains = ["go.boarddocs.com"]
+    start_urls = ["https://go.boarddocs.com/oh/akron/Board.nsf/XML-ActiveMeetings"]
+    location = {
+        "name": "Sylvester Small Administration Building",
+        "address": "10 N Main St, Akron, OH 44308",
+    }
+
+    def parse(self, response):
+        """
+        `parse` should always `yield` Meeting items.
+
+        Change the `_parse_title`, `_parse_start`, etc methods to fit your scraping
+        needs.
+        """
+        # Getting around broken XML response by breaking into chunks
+        meetings_split = (
+            response.text.split("<meetings>")[1].split("</meetings>")[0].split("</meeting>")
+        )
+        meeting_items = [Selector(text=m + "</meeting>").xpath("//meeting") for m in meetings_split]
+        filtered_meetings = [
+            m for m in meeting_items if m.xpath("start/date/text()").extract_first()
+            and "Archive" not in (m.xpath("description/text()").extract_first() or "")
+        ]
+        for idx, item in enumerate(
+            sorted(
+                filtered_meetings,
+                key=lambda m: m.xpath("start/date/text()").extract_first(),
+                reverse=True,
+            )
+        ):
+            if idx == 0:
+                yield from self._yield_next_meeting(item)
+            self._validate_location(item)
+            start = self._parse_start(item)
+            if not start:
+                continue
+            title = self._parse_title(item)
+            source = self._parse_source(item)
+            meeting = Meeting(
+                title=title,
+                description="",
+                classification=self._parse_classification(title),
+                start=start,
+                end=None,
+                time_notes="",
+                all_day=False,
+                location=self.location,
+                links=self._parse_links(source),
+                source=source or "https://go.boarddocs.com/oh/akron/Board.nsf/Public",
+            )
+            meeting["status"] = self._get_status(
+                meeting, text=item.xpath("description/text()").extract_first() or ""
+            )
+            meeting["id"] = self._get_id(meeting)
+            yield meeting
+
+    def _yield_next_meeting(self, item):
+        """Parse next meeting if available"""
+        dt_matches = re.findall(
+            r"[A-Z][a-z]{2,8} \d{1,2}, \d{4}, at \d{1,2}:\d{2} [apm\.]{2,4}",
+            item.xpath("description/text()").extract_first()
+        )
+        if len(dt_matches) > 1:
+            for dt_str in dt_matches[1:]:
+                meeting = Meeting(
+                    title="Board of Education",
+                    description="",
+                    classification=BOARD,
+                    start=datetime.strptime(dt_str.replace(".", ""), "%B %d, %Y, at %I:%M %p"),
+                    end=None,
+                    time_notes="",
+                    all_day=False,
+                    location=self.location,
+                    links=[],
+                    source="https://go.boarddocs.com/oh/akron/Board.nsf/Public",
+                )
+                meeting["status"] = self._get_status(meeting)
+                meeting["id"] = self._get_id(meeting)
+                yield meeting
+
+    def _parse_title(self, item):
+        """Parse or generate meeting title."""
+        title_str = item.xpath("name/text()").extract_first()
+        if "board" in title_str.lower():
+            return "Board of Education"
+        return re.split(r'\s+at\s+\d', title_str)[0].replace("Meeting", "").strip()
+
+    def _parse_classification(self, title):
+        """Parse or generate classification from allowed options."""
+        if "Board" in title:
+            return BOARD
+        return COMMITTEE
+
+    def _parse_start(self, item):
+        """Parse start datetime as a naive datetime object."""
+        date_str = (item.xpath('start/date/text()').extract_first() or "").strip()
+        if not date_str:
+            return
+        desc = item.xpath('description/text()').extract_first() or ""
+        time_match = re.search(r"\d{1,2}:\d{2} [apmAPM\.]{2,4}", desc)
+        time_str = "12:00 am"
+        if time_match:
+            time_str = time_match.group().replace(".", "").lower()
+        return datetime.strptime(date_str + time_str, "%Y-%m-%d%I:%M %p")
+
+    def _validate_location(self, item):
+        """Parse or generate location."""
+        if "Main St" not in item.xpath("description/text()").extract_first():
+            raise ValueError("Meeting location has changed")
+
+    def _parse_links(self, source):
+        """Parse or generate links."""
+        links = []
+        if source:
+            links.append({
+                "title": "Agenda",
+                "href": source,
+            })
+        return links
+
+    def _parse_source(self, item):
+        """Parse or generate source."""
+        link_match = re.search(r"(?<=\<link\>).*?(?=[\<\n\s])", item.extract()[0])
+        if link_match:
+            return link_match.group()
---
+++ tests/test_akr_public_schools.py
@@ -0,0 +1,80 @@
+from datetime import datetime
+from os.path import dirname, join
+
+import pytest  # noqa
+from city_scrapers_core.constants import BOARD, PASSED
+from city_scrapers_core.utils import file_response
+from freezegun import freeze_time
+
+from city_scrapers.spiders.akr_public_schools import AkrPublicSchoolsSpider
+
+test_response = file_response(
+    join(dirname(__file__), "files", "akr_public_schools.xml"),
+    url="https://go.boarddocs.com/oh/akron/Board.nsf/XML-ActiveMeetings",
+)
+spider = AkrPublicSchoolsSpider()
+
+freezer = freeze_time("2019-09-27")
+freezer.start()
+
+parsed_items = [item for item in spider.parse(test_response)]
+
+freezer.stop()
+
+
+def test_count():
+    assert len(parsed_items) == 7
+
+
+def test_title():
+    assert parsed_items[0]["title"] == "Board of Education"
+
+
+def test_description():
+    assert parsed_items[0]["description"] == ""
+
+
+def test_start():
+    assert parsed_items[0]["start"] == datetime(2019, 10, 7, 17, 30)
+
+
+def test_end():
+    assert parsed_items[0]["end"] is None
+
+
+def test_time_notes():
+    assert parsed_items[0]["time_notes"] == ""
+
+
+def test_id():
+    assert parsed_items[0]["id"] == "akr_public_schools/201910071730/x/board_of_education"
+
+
+def test_status():
+    assert parsed_items[1]["status"] == PASSED
+
+
+def test_location():
+    assert parsed_items[0]["location"] == spider.location
+
+
+def test_source():
+    assert parsed_items[0]["source"] == "https://go.boarddocs.com/oh/akron/Board.nsf/Public"
+    assert parsed_items[1][
+        "source"] == "http://go.boarddocs.com/oh/akron/Board.nsf/goto?open&amp;id=BFPP3S6262FE"
+
+
+def test_links():
+    assert parsed_items[0]["links"] == []
+    assert parsed_items[1]["links"] == [{
+        "href": "http://go.boarddocs.com/oh/akron/Board.nsf/goto?open&amp;id=BFPP3S6262FE",
+        "title": "Agenda"
+    }]
+
+
+def test_classification():
+    assert parsed_items[0]["classification"] == BOARD
+
+
+def test_all_day():
+    assert parsed_items[0]["all_day"] is False
