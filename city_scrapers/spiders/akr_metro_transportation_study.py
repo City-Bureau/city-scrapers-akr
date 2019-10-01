9bb4a4d060870215125fe518cb403b3dbd15ffb3
pjsier <pjsier@gmail.com>
2019-10-01 10:48:08 -0500

    Add akr_metro_transportation_study

---
+++ city_scrapers/spiders/akr_metro_transportation_study.py
@@ -0,0 +1,112 @@
+import json
+import re
+from collections import defaultdict
+from datetime import datetime
+
+import scrapy
+from city_scrapers_core.constants import ADVISORY_COMMITTEE
+from city_scrapers_core.items import Meeting
+from city_scrapers_core.spiders import CityScrapersSpider
+from dateutil.relativedelta import relativedelta
+
+
+class AkrMetroTransportationStudySpider(CityScrapersSpider):
+    name = "akr_metro_transportation_study"
+    agency = "Akron Metropolitan Area Transportation Study"
+    timezone = "America/Detroit"
+    allowed_domains = ["amatsplanning.org"]
+    start_urls = ["http://amatsplanning.org/category/meetings/"]
+
+    def parse(self, response):
+        self.month_link_map = self._parse_archive(response)
+        this_month = datetime.now().replace(day=1)
+        for month in [this_month + relativedelta(months=m) for m in range(-2, 3)]:
+            yield scrapy.Request(
+                "http://amatsplanning.org/calendar/{}/".format(month.strftime("%Y-%m")),
+                callback=self._parse_calendar,
+                dont_filter=True,
+            )
+
+    def _parse_archive(self, response):
+        month_link_map = defaultdict(list)
+        for section in response.css(".large-12.archive-subsection"):
+            month_match = re.search(
+                r"[A-Z][a-z]{2,8} \d{4}",
+                section.css("h2::text").extract_first() or ""
+            )
+            if not month_match:
+                continue
+            month_year = month_match.group()
+            for link in section.css("a"):
+                link_title = " ".join(link.css("*::text").extract())
+                if "Packet" in link_title:
+                    link_title = "Meeting Packet"
+                month_link_map[month_year].append({
+                    "title": link_title,
+                    "href": response.urljoin(link.attrib["href"]),
+                })
+        return month_link_map
+
+    def _parse_calendar(self, response):
+        schema_json = response.css("script[type='application/ld+json']::text").extract()
+        if len(schema_json) < 2:
+            return
+        events = json.loads(schema_json[-1])
+        for event in events:
+            if "Committee" in event["name"]:
+                yield scrapy.Request(event["url"], callback=self._parse_event, dont_filter=True)
+
+    def _parse_event(self, response):
+        data = json.loads(response.css("script[type='application/ld+json']::text").extract()[-1])[0]
+        start = self._parse_dt(data.get("startDate"))
+        if not start:
+            return
+        meeting = Meeting(
+            title=data["name"],
+            description="",
+            classification=ADVISORY_COMMITTEE,
+            start=start,
+            end=self._parse_dt(data.get("endDate")),
+            all_day=False,
+            time_notes="",
+            location=self._parse_location(data),
+            links=self._parse_links(start, data["name"]),
+            source=response.url,
+        )
+
+        meeting["status"] = self._get_status(meeting)
+        meeting["id"] = self._get_id(meeting)
+
+        yield meeting
+
+    def _parse_dt(self, date_str):
+        if not date_str:
+            return
+        return datetime.strptime(date_str[0:-6], "%Y-%m-%dT%H:%M:%S")
+
+    def _parse_location(self, item):
+        """Parse or generate location."""
+        if not item.get("location"):
+            return {
+                "address": "",
+                "name": "TBD",
+            }
+
+        loc_obj = {
+            "name": item["location"]["name"],
+            "address": "",
+        }
+        if item["location"].get("address"):
+            loc_obj["address"] = " ".join([
+                item["location"]["address"].get(p)
+                for p in ["streetAddress", "addressLocality", "addressRegion", "postalCode"]
+                if item["location"]["address"].get(p)
+            ])
+        return loc_obj
+
+    def _parse_links(self, date_obj, title):
+        """Parse or generate links."""
+        return [
+            link for link in self.month_link_map[date_obj.strftime("%B %Y")]
+            if title.lower() in link["title"].lower() or "Packet" in link["title"]
+        ]
---
+++ tests/test_akr_metro_transportation_study.py
@@ -0,0 +1,89 @@
+from datetime import datetime
+from os.path import dirname, join
+
+import pytest  # noqa
+from city_scrapers_core.constants import ADVISORY_COMMITTEE, PASSED
+from city_scrapers_core.utils import file_response
+from freezegun import freeze_time
+
+from city_scrapers.spiders.akr_metro_transportation_study import AkrMetroTransportationStudySpider
+
+test_response = file_response(
+    join(dirname(__file__), "files", "akr_metro_transportation_study_detail.html"),
+    url="http://amatsplanning.org/calendars/technical-advisory-committee-9/",
+)
+test_docs_response = file_response(
+    join(dirname(__file__), "files", "akr_metro_transportation_study.html"),
+    url="http://amatsplanning.org/category/meetings/",
+)
+spider = AkrMetroTransportationStudySpider()
+
+freezer = freeze_time("2019-09-30")
+freezer.start()
+
+spider.month_link_map = spider._parse_archive(test_docs_response)
+parsed_item = [item for item in spider._parse_event(test_response)][0]
+
+freezer.stop()
+
+
+def test_title():
+    assert parsed_item["title"] == "Technical Advisory Committee"
+
+
+def test_description():
+    assert parsed_item["description"] == ""
+
+
+def test_start():
+    assert parsed_item["start"] == datetime(2019, 5, 9, 13, 30)
+
+
+def test_end():
+    assert parsed_item["end"] == datetime(2019, 5, 9, 16, 0)
+
+
+def test_time_notes():
+    assert parsed_item["time_notes"] == ""
+
+
+def test_id():
+    assert parsed_item[
+        "id"] == "akr_metro_transportation_study/201905091330/x/technical_advisory_committee"
+
+
+def test_status():
+    assert parsed_item["status"] == PASSED
+
+
+def test_location():
+    assert parsed_item["location"] == {
+        "name": "Hilton Garden Inn",
+        "address": "1307 E. Market St. Akron OH 44305"
+    }
+
+
+def test_source():
+    assert parsed_item["source"] == test_response.url
+
+
+def test_links():
+    assert parsed_item["links"] == [
+        {
+            "href":
+                "http://amatsplanning.org/wp-content/uploads/TAC-CIC-and-Policy-Committee-Meeting-Materials-May-2019.pdf",  # noqa
+            "title": "Meeting Packet"
+        },
+        {
+            "title": "Technical Advisory Committee Podcast",
+            "href": "http://amatsplanning.org/wp-content/uploads/TAC-5-9-19-1.mp3"
+        }
+    ]
+
+
+def test_classification():
+    assert parsed_item["classification"] == ADVISORY_COMMITTEE
+
+
+def test_all_day():
+    assert parsed_item["all_day"] is False
