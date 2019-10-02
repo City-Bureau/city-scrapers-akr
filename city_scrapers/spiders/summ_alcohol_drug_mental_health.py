b930b34f9b500e3974b0d84ec2d2c51985c39bf0
pjsier <pjsier@gmail.com>
2019-10-02 14:43:02 -0500

    Add summ_alcohol_drug_mental_health

---
+++ city_scrapers/spiders/summ_alcohol_drug_mental_health.py
@@ -0,0 +1,146 @@
+import json
+import re
+from collections import defaultdict
+from datetime import datetime, timedelta
+
+import pytz
+import scrapy
+from city_scrapers_core.constants import BOARD, COMMITTEE
+from city_scrapers_core.items import Meeting
+from city_scrapers_core.spiders import CityScrapersSpider
+
+
+class SummAlcoholDrugMentalHealthSpider(CityScrapersSpider):
+    name = "summ_alcohol_drug_mental_health"
+    agency = "Summit County Alcohol, Drug Addiction and Mental Health Services Board"
+    timezone = "America/Detroit"
+    allowed_domains = ["www.admboard.org"]
+    start_urls = ["https://www.admboard.org/board-of-directors.aspx"]
+
+    def parse(self, response):
+        self.date_link_map = self._parse_documents(response)
+        since = datetime.now() - timedelta(days=120)
+        since_epoch = str(int(since.timestamp() * 1000))
+        yield scrapy.Request(
+            "https://tockify.com/api/ngevent?view=agenda&calname=jackstest&start-inclusive=true&longForm=false&showAll=false&search=board&startms="  # noqa
+            + since_epoch,
+            callback=self._parse_calendar,
+            dont_filter=True
+        )
+
+    def _parse_documents(self, response):
+        date_link_map = defaultdict(list)
+        for link in response.css(".modulecontent li a"):
+            link_str = " ".join(link.css("*::text").extract())
+            date_match = re.search(r"[A-Z][a-z]{2,8},? \d{4}", link_str)
+            if not date_match:
+                continue
+            date_str = date_match.group().replace(",", "")
+            date_link_map[date_str].append({
+                "title": "Minutes",
+                "href": response.urljoin(link.attrib["href"]),
+            })
+        return date_link_map
+
+    def _parse_calendar(self, response):
+        data = json.loads(response.text)
+        for item in data["events"]:
+            yield scrapy.Request(
+                "https://tockify.com/jackstest/detail/{}/{}".format(
+                    item["eid"]["uid"], item["eid"]["tid"]
+                ),
+                callback=self._parse_event,
+                dont_filter=True,
+            )
+
+    def _parse_event(self, response):
+        """
+        `_parse_calendar` should always `yield` Meeting items.
+
+        Change the `_parse_title`, `_parse_start`, etc methods to fit your scraping
+        needs.
+        """
+        data = json.loads(response.css("script[type='application/ld+json']::text").extract_first())
+        detail = {}
+        script_str = " ".join(response.css("head script[type='text/javascript']::text").extract())
+        json_match = re.search(r"(?<=window\.tkf = ).*?(?=;\n)", script_str)
+        if json_match:
+            detail = json.loads(json_match.group())
+            events = detail.get("bootdata", {}).get("query", {}).get("detail", {}).get("events", [])
+            if len(events) > 0:
+                detail = events[0]
+
+        title = self._parse_title(data)
+        classification = self._parse_classification(title)
+        start = self._parse_dt(data["startDate"])
+        links = []
+        if classification == BOARD:
+            links = self.date_link_map[start.strftime("%B %Y")]
+        meeting = Meeting(
+            title=title,
+            description="",
+            classification=classification,
+            start=start,
+            end=self._parse_dt(data["endDate"]),
+            all_day=False,
+            time_notes="",
+            location=self._parse_location(detail),
+            links=links + self._parse_links(detail),
+            source=response.url,
+        )
+
+        meeting["status"] = self._get_status(meeting, text=data["name"])
+        meeting["id"] = self._get_id(meeting)
+
+        yield meeting
+
+    def _parse_title(self, item):
+        """Parse or generate meeting title."""
+        if "Regular" in item["name"]:
+            return "Board of Directors"
+        if "Committee" in item["name"]:
+            title_str = re.search(r".*?Committee", item["name"]).group()
+            return re.sub(r"Board of Directors(\')?\s+", "", title_str).replace("ad hoc",
+                                                                                "Ad Hoc").strip()
+        return re.sub(r"postpone(\w+)\s+-?\s+", "", item["name"], flags=re.I).strip()
+
+    def _parse_classification(self, title):
+        """Parse or generate classification from allowed options."""
+        if "Committee" in title:
+            return COMMITTEE
+        return BOARD
+
+    def _parse_dt(self, dt_str):
+        """Parse datetime string in UTC and convert it to a naive datetime in local time"""
+        dt = datetime.strptime(dt_str, "%Y-%m-%dT%H:%M:%SZ")
+        tz = pytz.timezone(self.timezone)
+        return pytz.utc.localize(dt).astimezone(tz).replace(tzinfo=None)
+
+    def _parse_location(self, detail):
+        """Parse or generate location."""
+        return {
+            "name": detail["content"].get("place", ""),
+            "address": detail["content"].get("address", ""),
+        }
+
+    def _parse_links(self, detail):
+        """Parse or generate links."""
+        links = []
+        desc_str = detail["content"]["description"].get("text", "")
+        desc_el = scrapy.Selector(text=desc_str)
+        for link in desc_el.css("a"):
+            link_title = " ".join(link.css("*::text").extract()).strip()
+            link_href = link.attrib["href"]
+            if "mailto" in link_href:
+                continue
+            if "agenda" in link_href.lower():
+                link_title = "Agenda"
+            elif "notice" in link_href.lower():
+                link_title = "Notice"
+            else:
+                link_title = "Materials"
+            links.append({
+                "title": link_title,
+                "href": link_href,
+            })
+        return links
---
+++ tests/test_summ_alcohol_drug_mental_health.py
@@ -0,0 +1,89 @@
+from datetime import datetime
+from os.path import dirname, join
+
+import pytest  # noqa
+from city_scrapers_core.constants import BOARD, PASSED
+from city_scrapers_core.utils import file_response
+from freezegun import freeze_time
+
+from city_scrapers.spiders.summ_alcohol_drug_mental_health import SummAlcoholDrugMentalHealthSpider
+
+test_docs_response = file_response(
+    join(dirname(__file__), "files", "summ_alcohol_drug_mental_health_minutes.html"),
+    url="https://www.admboard.org/board-of-directors.aspx",
+)
+test_response = file_response(
+    join(dirname(__file__), "files", "summ_alcohol_drug_mental_health.html"),
+    url="https://tockify.com/jackstest/detail/249/1563917400000",
+)
+spider = SummAlcoholDrugMentalHealthSpider()
+
+freezer = freeze_time("2019-10-02")
+freezer.start()
+
+spider.date_link_map = spider._parse_documents(test_docs_response)
+parsed_item = [item for item in spider._parse_event(test_response)][0]
+
+freezer.stop()
+
+
+def test_title():
+    assert parsed_item["title"] == "Board of Directors"
+
+
+def test_description():
+    assert parsed_item["description"] == ""
+
+
+def test_start():
+    assert parsed_item["start"] == datetime(2019, 7, 23, 17, 30)
+
+
+def test_end():
+    assert parsed_item["end"] == datetime(2019, 7, 23, 19, 30)
+
+
+def test_time_notes():
+    assert parsed_item["time_notes"] == ""
+
+
+def test_id():
+    assert parsed_item["id"] == "summ_alcohol_drug_mental_health/201907231730/x/board_of_directors"
+
+
+def test_status():
+    assert parsed_item["status"] == PASSED
+
+
+def test_location():
+    assert parsed_item["location"] == {
+        "name": "Summit County Public Health Department Board Room",
+        "address": "1867 W. Market Street, Entrance A, Akron, OH 44313",
+    }
+
+
+def test_source():
+    assert parsed_item["source"] == test_response.url
+
+
+def test_links():
+    assert parsed_item["links"] == [
+        {
+            "href":
+                "https://www.admboard.org/Data/Sites/25/Assets/pdfs/BOD Minutes/bod-min-7.23.19-final-app-09.24.19.pdf",  # noqa
+            "title": "Minutes"
+        },
+        {
+            "title": "Agenda",
+            "href":
+                "https://www.admboard.org/Data/Sites/25/adm-bod-mtg-notice-agenda-pkt-7.23.19.pdf"
+        }
+    ]
+
+
+def test_classification():
+    assert parsed_item["classification"] == BOARD
+
+
+def test_all_day():
+    assert parsed_item["all_day"] is False
