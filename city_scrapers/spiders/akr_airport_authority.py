9a857429410e7e79b91abf6a1886d32820827466
pjsier <pjsier@gmail.com>
2019-10-03 18:37:30 -0500

    Add akr_airport_authority

---
+++ city_scrapers/spiders/akr_airport_authority.py
@@ -0,0 +1,68 @@
+import re
+from datetime import datetime
+from email import policy
+from email.parser import BytesParser
+
+from city_scrapers_core.constants import BOARD
+from city_scrapers_core.items import Meeting
+from city_scrapers_core.spiders import CityScrapersSpider
+
+
+class AkrAirportAuthoritySpider(CityScrapersSpider):
+    name = "akr_airport_authority"
+    agency = "Akron-Canton Airport Authority"
+    timezone = "America/Detroit"
+    allowed_domains = ["city-scrapers-notice-emails.s3.amazonaws.com"]
+    start_urls = [
+        "https://city-scrapers-notice-emails.s3.amazonaws.com/akr_airport_authority/latest.eml"
+    ]
+    location = {
+        "name": "",
+        "address": "5400 Lauby Rd. NW #9 North Canton, OH 44720",
+    }
+
+    def parse(self, response):
+        """
+        `parse` should always `yield` Meeting items.
+
+        Change the `_parse_title`, `_parse_start`, etc methods to fit your scraping
+        needs.
+        """
+        email_parser = BytesParser(policy=policy.default)
+        parsed_email = email_parser.parsebytes(response.body)
+        content = ""
+        for part in parsed_email.iter_parts():
+            if part.get_content_maintype() == "multipart":
+                for sub_part in part.get_payload():
+                    if sub_part.get_content_maintype() == "text":
+                        content = sub_part.get_content()
+                        break
+        year_str = re.search(r"\d{4}", content).group()
+        for date_str in re.findall(r"[A-Z][a-z]{2,8} \d{1,2}", content):
+            start = self._parse_start(date_str, year_str)
+            if not start:
+                continue
+            meeting = Meeting(
+                title="Board of Trustees",
+                description="",
+                classification=BOARD,
+                start=start,
+                end=None,
+                all_day=False,
+                time_notes="Confirm meeting details with agency",
+                location=self.location,
+                links=[],
+                source=response.url,
+            )
+
+            meeting["status"] = self._get_status(meeting)
+            meeting["id"] = self._get_id(meeting)
+
+            yield meeting
+
+    def _parse_start(self, date_str, year_str):
+        """Parse start datetime as a naive datetime object."""
+        try:
+            return datetime.strptime(date_str + year_str + "15", "%B %d%Y%H")
+        except ValueError:
+            return
---
+++ tests/test_akr_airport_authority.py
@@ -0,0 +1,76 @@
+from datetime import datetime
+from os.path import dirname, join
+
+import pytest  # noqa
+from city_scrapers_core.constants import BOARD, TENTATIVE
+from city_scrapers_core.utils import file_response
+from freezegun import freeze_time
+
+from city_scrapers.spiders.akr_airport_authority import AkrAirportAuthoritySpider
+
+test_response = file_response(
+    join(dirname(__file__), "files", "akr_airport_authority.eml"),
+    url="https://city-scrapers-notice-emails.s3.amazonaws.com/akr_airport_authority/latest.eml",
+)
+spider = AkrAirportAuthoritySpider()
+
+freezer = freeze_time("2019-10-03")
+freezer.start()
+
+parsed_items = [item for item in spider.parse(test_response)]
+
+freezer.stop()
+
+
+def test_count():
+    assert len(parsed_items) == 3
+
+
+def test_title():
+    assert parsed_items[0]["title"] == "Board of Trustees"
+
+
+def test_description():
+    assert parsed_items[0]["description"] == ""
+
+
+def test_start():
+    assert parsed_items[0]["start"] == datetime(2019, 10, 17, 15, 0)
+
+
+def test_end():
+    assert parsed_items[0]["end"] is None
+
+
+def test_time_notes():
+    assert parsed_items[0]["time_notes"] == "Confirm meeting details with agency"
+
+
+def test_id():
+    assert parsed_items[0]["id"] == "akr_airport_authority/201910171500/x/board_of_trustees"
+
+
+def test_status():
+    assert parsed_items[0]["status"] == TENTATIVE
+
+
+def test_location():
+    assert parsed_items[0]["location"] == spider.location
+
+
+def test_source():
+    assert parsed_items[0][
+        "source"
+    ] == "https://city-scrapers-notice-emails.s3.amazonaws.com/akr_airport_authority/latest.eml"
+
+
+def test_links():
+    assert parsed_items[0]["links"] == []
+
+
+def test_classification():
+    assert parsed_items[0]["classification"] == BOARD
+
+
+def test_all_day():
+    assert parsed_items[0]["all_day"] is False
