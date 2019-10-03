1d38c9295bfe4d8e870af69ec8d5a5649d71900b
pjsier <pjsier@gmail.com>
2019-10-02 21:57:02 -0500

    Add summ_board_control

---
+++ city_scrapers/spiders/summ_board_control.py
@@ -0,0 +1,72 @@
+from collections import defaultdict
+from datetime import datetime
+
+from city_scrapers_core.constants import BOARD
+from city_scrapers_core.items import Meeting
+from city_scrapers_core.spiders import CityScrapersSpider
+
+
+class SummBoardControlSpider(CityScrapersSpider):
+    name = "summ_board_control"
+    agency = "Summit County Board of Control"
+    timezone = "America/Detroit"
+    allowed_domains = ["co.summitoh.net"]
+    start_urls = [
+        "https://co.summitoh.net/index.php/offices/boards-agencies-a-commissions/board-of-control"
+    ]
+    location = {
+        "name": "Council Chambers",
+        "address": "175 S Main St, Floor 7, Akron, OH 44308",
+    }
+
+    def parse(self, response):
+        """
+        `parse` should always `yield` Meeting items.
+
+        Change the `_parse_title`, `_parse_start`, etc methods to fit your scraping
+        needs.
+        """
+        date_link_map = defaultdict(list)
+
+        for link in response.css("#sidebar a"):
+            if (
+                "agenda" not in link.attrib["href"].lower()
+                and "minutes" not in link.attrib["href"].lower()
+            ):
+                continue
+            link_title = "Agenda" if "agenda" in link.attrib["href"].lower() else "Minutes"
+            start = self._parse_start(link)
+            if not start:
+                continue
+            date_link_map[start].append({
+                "title": link_title,
+                "href": response.urljoin(link.attrib["href"]),
+            })
+
+        start_list = sorted([k for k in date_link_map.keys() if k], reverse=True)
+        # Use the most recent 20 meetings
+        for start in start_list[:20]:
+            meeting = Meeting(
+                title="Board of Control",
+                description="",
+                classification=BOARD,
+                start=start,
+                end=None,
+                all_day=False,
+                time_notes="See agenda to confirm details",
+                location=self.location,
+                links=date_link_map[start],
+                source=response.url,
+            )
+
+            meeting["status"] = self._get_status(meeting)
+            meeting["id"] = self._get_id(meeting)
+
+            yield meeting
+
+    def _parse_start(self, item):
+        """Parse start datetime as a naive datetime object."""
+        date_str = " ".join(item.css("*::text").extract()).strip()
+        if not date_str:
+            return
+        return datetime.strptime(date_str + "1030", "%m-%d-%y%H%M")
---
+++ tests/test_summ_board_control.py
@@ -0,0 +1,78 @@
+from datetime import datetime
+from os.path import dirname, join
+
+import pytest  # noqa
+from city_scrapers_core.constants import BOARD, TENTATIVE
+from city_scrapers_core.utils import file_response
+from freezegun import freeze_time
+
+from city_scrapers.spiders.summ_board_control import SummBoardControlSpider
+
+test_response = file_response(
+    join(dirname(__file__), "files", "summ_board_control.html"),
+    url="https://co.summitoh.net/index.php/offices/boards-agencies-a-commissions/board-of-control",
+)
+spider = SummBoardControlSpider()
+
+freezer = freeze_time("2019-10-02")
+freezer.start()
+
+parsed_items = [item for item in spider.parse(test_response)]
+
+freezer.stop()
+
+
+def test_count():
+    assert len(parsed_items) == 20
+
+
+def test_title():
+    assert parsed_items[0]["title"] == "Board of Control"
+
+
+def test_description():
+    assert parsed_items[0]["description"] == ""
+
+
+def test_start():
+    assert parsed_items[0]["start"] == datetime(2019, 10, 2, 10, 30)
+
+
+def test_end():
+    assert parsed_items[0]["end"] is None
+
+
+def test_time_notes():
+    assert parsed_items[0]["time_notes"] == "See agenda to confirm details"
+
+
+def test_id():
+    assert parsed_items[0]["id"] == "summ_board_control/201910021030/x/board_of_control"
+
+
+def test_status():
+    assert parsed_items[0]["status"] == TENTATIVE
+
+
+def test_location():
+    assert parsed_items[0]["location"] == spider.location
+
+
+def test_source():
+    assert parsed_items[0]["source"] == test_response.url
+
+
+def test_links():
+    assert parsed_items[0]["links"] == [{
+        "href":
+            "https://co.summitoh.net/images/stories/Finance/BOC/Agenda/19Agenda/10-02-19BOCAGENDA.pdf",  # noqa
+        "title": "Agenda"
+    }]
+
+
+def test_classification():
+    assert parsed_items[0]["classification"] == BOARD
+
+
+def test_all_day():
+    assert parsed_items[0]["all_day"] is False
