fdb8153729f9d2bce11e128a44434e0963ab0f59
Daniel Simmons-Ritchie <37225902+SimmonsRitchie@users.noreply.github.com>
2024-05-07 14:00:13 -0500

    Fix: summ_alcohol_drug_mental_health

--- city_scrapers/spiders/summ_alcohol_drug_mental_health.py
+++ city_scrapers/spiders/summ_alcohol_drug_mental_health.py
@@ -1,155 +1,63 @@
 import json
-import re
-from collections import defaultdict
-from datetime import datetime, timedelta
 
-import pytz
-import scrapy
-from city_scrapers_core.constants import BOARD, COMMITTEE
+from city_scrapers_core.constants import BOARD
 from city_scrapers_core.items import Meeting
 from city_scrapers_core.spiders import CityScrapersSpider
+from dateutil.parser import parse
+from w3lib.html import remove_tags
 
 
 class SummAlcoholDrugMentalHealthSpider(CityScrapersSpider):
     name = "summ_alcohol_drug_mental_health"
     agency = "Summit County Alcohol, Drug Addiction and Mental Health Services Board"
     timezone = "America/Detroit"
-    start_urls = ["https://www.admboard.org/board-of-directors.aspx"]
+    start_urls = ["https://admboard.org/board-of-directors/"]
 
     def parse(self, response):
-        self.date_link_map = self._parse_documents(response)
-        since = datetime.now() - timedelta(days=120)
-        since_epoch = str(int(since.timestamp() * 1000))
-        yield scrapy.Request(
-            "https://tockify.com/api/ngevent?view=agenda&calname=jackstest&start-inclusive=true&longForm=false&showAll=false&search=board&startms="  # noqa
-            + since_epoch,
-            callback=self._parse_calendar,
-            dont_filter=True,
-        )
+        # Extract JSON-LD script content
+        json_ld_scripts = response.xpath('//script[@type="application/ld+json"]/text()')
+        for json_ld in json_ld_scripts:
+            json_ld_content = json_ld.extract()
+            data = json.loads(json_ld_content)
+            if data["@type"] == "Event":
+                yield from self.parse_event(data)
 
-    def _parse_documents(self, response):
-        date_link_map = defaultdict(list)
-        for link in response.css(".modulecontent li a"):
-            link_str = " ".join(link.css("*::text").extract())
-            date_match = re.search(r"[A-Z][a-z]{2,8},? \d{4}", link_str)
-            if not date_match:
-                continue
-            date_str = date_match.group().replace(",", "")
-            date_link_map[date_str].append(
-                {"title": "Minutes", "href": response.urljoin(link.attrib["href"])}
-            )
-        return date_link_map
+    def parse_event(self, data):
+        title = data["name"]
+        description = remove_tags(data.get("description", ""))
+        start = self.parse_datetime(data["startDate"])
+        end = self.parse_datetime(data["endDate"])
+        location = self.parse_location(data)
+        links = [{"title": "Event Details", "href": data["url"]}]
 
-    def _parse_calendar(self, response):
-        data = json.loads(response.text)
-        for item in data["events"]:
-            yield scrapy.Request(
-                "https://tockify.com/jackstest/detail/{}/{}".format(
-                    item["eid"]["uid"], item["eid"]["tid"]
-                ),
-                callback=self._parse_event,
-                dont_filter=True,
-            )
-
-    def _parse_event(self, response):
-        """
-        `_parse_calendar` should always `yield` Meeting items.
-
-        Change the `_parse_title`, `_parse_start`, etc methods to fit your scraping
-        needs.
-        """
-        data = json.loads(
-            response.css("script[type='application/ld+json']::text").extract_first()
-        )
-        detail = {}
-        script_str = " ".join(
-            response.css("head script[type='text/javascript']::text").extract()
-        )
-        json_match = re.search(r"(?<=window\.tkf = ).*?(?=;\n)", script_str)
-        if json_match:
-            detail = json.loads(json_match.group())
-            events = (
-                detail.get("bootdata", {})
-                .get("query", {})
-                .get("detail", {})
-                .get("events", [])
-            )
-            if len(events) > 0:
-                detail = events[0]
-
-        title = self._parse_title(data)
-        classification = self._parse_classification(title)
-        start = self._parse_dt(data["startDate"])
-        links = []
-        if classification == BOARD:
-            links = self.date_link_map[start.strftime("%B %Y")]
         meeting = Meeting(
             title=title,
-            description="",
-            classification=classification,
+            description=description,
+            classification=BOARD,
             start=start,
-            end=self._parse_dt(data["endDate"]),
+            end=end,
             all_day=False,
             time_notes="",
-            location=self._parse_location(detail),
-            links=links + self._parse_links(detail),
-            source=response.url,
+            location=location,
+            links=links,
+            source=data["url"],
         )
-
-        meeting["status"] = self._get_status(meeting, text=data["name"])
+        meeting["status"] = self._get_status(meeting, text=data["eventStatus"])
         meeting["id"] = self._get_id(meeting)
-
         yield meeting
 
-    def _parse_title(self, item):
-        """Parse or generate meeting title."""
-        if "Regular" in item["name"]:
-            return "Board of Directors"
-        if "Committee" in item["name"]:
-            title_str = re.search(r".*?Committee", item["name"]).group()
-            return (
-                re.sub(r"Board of Directors(\')?\s+", "", title_str)
-                .replace("ad hoc", "Ad Hoc")
-                .strip()
-            )
-        return re.sub(r"postpone(\w+)\s+-?\s+", "", item["name"], flags=re.I).strip()
+    def parse_datetime(self, dt_str):
+        # Use dateutil's parse to automatically handle different datetime formats
+        return parse(dt_str).replace(tzinfo=None)
 
-    def _parse_classification(self, title):
-        """Parse or generate classification from allowed options."""
-        if "Committee" in title:
-            return COMMITTEE
-        return BOARD
-
-    def _parse_dt(self, dt_str):
-        """
-        Parse datetime string in UTC and convert it to a naive datetime in local time
-        """
-        dt = datetime.strptime(dt_str.replace(":", ""), "%Y-%m-%dT%H%M%S%z")
-        tz = pytz.timezone(self.timezone)
-        return dt.astimezone(tz).replace(tzinfo=None)
-
-    def _parse_location(self, detail):
-        """Parse or generate location."""
+    def parse_location(self, data):
+        location_info = data.get("location", [{}])[0]
+        if not location_info:
+            return {
+                "name": "TBD",
+                "address": "",
+            }
         return {
-            "name": detail["content"].get("place", ""),
-            "address": detail["content"].get("address", ""),
+            "name": location_info.get("name", ""),
+            "address": location_info.get("address", {}).get("streetAddress", ""),
         }
-
-    def _parse_links(self, detail):
-        """Parse or generate links."""
-        links = []
-        desc_str = detail["content"]["description"].get("text", "")
-        desc_el = scrapy.Selector(text=desc_str)
-        for link in desc_el.css("a"):
-            link_title = " ".join(link.css("*::text").extract()).strip()
-            link_href = link.attrib["href"]
-            if "mailto" in link_href:
-                continue
-            if "agenda" in link_href.lower():
-                link_title = "Agenda"
-            elif "notice" in link_href.lower():
-                link_title = "Notice"
-            else:
-                link_title = "Materials"
-            links.append({"title": link_title, "href": link_href})
-        return links
