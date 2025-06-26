2dc7112b83734b66c1a99530a1b2ed890ef4f1f1
Duong Cao <duong.cao@eastagile.com>
2025-06-26 10:01:59 +0700

    [f] DOC-1237: test fix spider
    https://linear.app/pdw/issue/DOC-1237/metro-rta-agency-meetings-all-automatically-cancelled-for-unknown

--- .deploy.sh
+++ .deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-pipenv run scrapy list | xargs -I {} pipenv run scrapy crawl {} -s LOG_ENABLED=True &
+echo "Starting akr_metro_regional_transit with proxy..."
+env HTTP_PROXY="$HTTP_PROXY" HTTPS_PROXY="$HTTPS_PROXY" \
+pipenv run scrapy crawl akr_metro_regional_transit -s LOG_ENABLED=True
+
+echo "🚀 Starting other spiders WITHOUT proxy..."
+pipenv run scrapy list | grep -v akr_metro_regional_transit | \
+xargs -I {} pipenv run scrapy crawl {} -s LOG_ENABLED=True &
 
 # Output to the screen every 9 minutes to prevent a travis timeout
 # https://stackoverflow.com/a/40800348
